1207755dcb118361612697c7287f831ebcf36c96
kuldeeppaliwal <kuldeeppaliwal@hotmail.com>
2023-03-16 01:33:01 +0530

    Angular Pagination Changes with size.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPagination';
+  POSTS: any;
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 10;
+  tableSizes: any = [5, 10, 15, 20];
+  constructor(private userService: UsersService) { }
+  ngOnInit() {
+    this.postList();
+  }
+  postList(): void {
+    this.userService.getAllPosts().subscribe((response) => {
+      this.POSTS = response;
+      console.log(this.POSTS);
+    })
+  }
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.postList();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.postList();
+  }
 }
